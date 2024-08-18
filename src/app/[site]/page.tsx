41b8f8386b0a681f86450c0fb0843366ad336cf2
Super7000 <subratachowdhurysuper@gmail.com>
2024-08-18 23:17:48 +0530

    remove some un nessesary code and added auth form submit handler

--- src/app/[site]/page.tsx
+++ src/app/[site]/page.tsx
@@ -1,10 +1,19 @@
+'use client'
+import { useRouter } from "next/navigation";
 import AuthForm from "./components/AuthForm";
 
 export default function LandingPage() {
+    const router = useRouter()
+
+    const onSubmitHandler = (data: { firstName: string, lastName: string, email: string, password: string, confirmPassword: string }) => {
+        console.log(data)
+        router.push("/resume/builder")
+    }
+
     return (
         <div className="auth-container flex flex-col items-stretch lg:items-center justify-center h-screen">
             <div className="rounded-md border-2 border-gray-300">
-                <AuthForm />
+                <AuthForm onSubmit={onSubmitHandler} />
             </div>
         </div>
     )
